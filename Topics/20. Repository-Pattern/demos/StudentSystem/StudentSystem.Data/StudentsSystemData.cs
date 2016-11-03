b91aa82db7be56d42f0c945b2f89c897ecea4d95
Marto Vesheff <martin.veshev@gmail.com>
2016-11-03 02:09:29 +0200

    Fixed demos

---
+++ Topics/20. Repository-Pattern/demos/StudentSystem/StudentSystem.Data/StudentsSystemData.cs
@@ -0,0 +1,31 @@
+﻿namespace StudentSystem.Data
+{
+    using StudentSystem.Data.Repositories;
+    using StudentSystem.Models;
+    using System;
+    using System.Collections.Generic;
+
+    public class StudentsSystemData : IStudentSystemData, IDisposable
+    {
+        private IStudentSystemDbContext context;
+
+        public StudentsSystemData(IStudentSystemDbContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentException();
+            }
+
+            this.context = context;
+        }
+
+        public void Commit()
+        {
+            this.context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ Topics/20. Repository-Pattern/demos/StudentSystem/StudentSystem.Web/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using StudentSystem.Data;
+using StudentSystem.Data.Repositories;
+using StudentSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentSystem.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Func<IStudentSystemData> unitOfWorkFactory;
+        private IGenericRepository<Student> studentsRepo;
+        private IGenericRepository<Homework> homeworkRepo;
+
+        public HomeController(
+            Func<IStudentSystemData> unitOfWorkFactory,
+            IGenericRepository<Student> studentRepo,
+            IGenericRepository<Homework> homeworkRepo)
+        {
+            this.unitOfWorkFactory = unitOfWorkFactory;
+            this.studentsRepo = studentRepo;
+            this.homeworkRepo = homeworkRepo;
+        }
+
+        public ActionResult Index()
+        {
+            var students = this.studentsRepo.All.Where(x => this.homeworkRepo.All.FirstOrDefault().Student.FirstName == (x.FirstName));
+
+            return View(students);
+        }
+
+        [HttpPost]
+        public ActionResult Add(Student studentToAdd)
+        {
+            using (var unitOfWork = this.unitOfWorkFactory())
+            {
+                this.studentsRepo.Add(studentToAdd);
+                unitOfWork.Commit();
+            }
+
+            return this.Redirect("Details/" + studentToAdd.StudentIdentification);
+
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            var student = this.studentsRepo.GetById(id);
+
+            return this.View(student);
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return this.View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
