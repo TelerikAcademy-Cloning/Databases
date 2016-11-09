c998dac4cbef0b7d95aa9971c5d1eeab5bde2dd0
Doncho Minkov <donchominkov@gmail.com>
2016-11-09 11:15:02 +0200

    Added exam from 2016

--- Workshops/03. EntityFramework-Code-first/SocialNetwork/SocialNetwork.ConsoleClient/Startup.cs
+++ Workshops/03. EntityFramework-Code-first/SocialNetwork/SocialNetwork.ConsoleClient/Startup.cs
@@ -1,4 +1,10 @@
-﻿namespace SocialNetwork.ConsoleClient
+﻿using System.Data.Entity.Migrations;
+using MyWebApi.Common.Extensions;
+using SocialNetwork.ConsoleClient.Importers;
+using SocialNetwork.Data;
+using SocialNetwork.Models;
+
+namespace SocialNetwork.ConsoleClient
 {
     using System;
     using System.Data.Entity;
@@ -9,6 +15,11 @@
     {
         public static void Main()
         {
+            var dbContext = new SocialNetworkDbContext();
+            var importer = new XmlImporter(dbContext);
+            var friendships = importer.ImportFriendshipFromXml("..\\..\\XmlFiles\\Friendships-Test.xml");
+            friendships.ForEach(friendship => dbContext.Friendships.Add(friendship));
+            dbContext.SaveChanges();
         }
     }
 }
